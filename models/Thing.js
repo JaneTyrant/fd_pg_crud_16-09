c455a8c53ef608bd4ad228cd51b5d67f701fb147
TyrantJane <nonstop00066@gmail.com>
2022-09-19 19:05:47 +0300

    Add Thing & its controller

---
+++ models/Thing.js
@@ -0,0 +1,64 @@
+class Thing {
+    static client = null;
+    static tableName = 'things';
+    static attributes = {
+        body: 'string',
+        authhor: 'string'
+    }
+
+    static async create(values){
+        const arrAttrs = Object.entries(this.attributes)
+        .filter(([attr, domain]) => attr in values)
+        .map(([attr, domain]) => attr); //[body, author]
+
+        const insertAttrStr = arrAttrs.map((attr) => `"${attr}"`).join(',');
+        const insertValuesStr = arrAttrs.map((attr) => {
+            const value = values[attr];
+            return typeof value === 'string' ? `'${value}'` : value;
+        }).join(',');
+
+        const {rows} = await this.client.query(`
+        INSERT INTO ${this.tableName}(${insertAttrStr})
+        VALUES (${insertValuesStr})
+        RETURNING *;`);
+        return rows;
+    }
+    static async findAll(){
+        const {rows} = await this.client.query(`
+        SELECT * 
+        FROM ${this.tableName};`);
+        return rows;
+    }
+    static async findByPk(pkValue){
+        const {rows} = await this.client.query(`
+        SELECT * 
+        FROM ${this.tableName} 
+        WHERE "id"=${pkValue};`);
+        return rows;
+    }
+    static async updateByPk(values, pkValue){
+        const arrAttrs = Object.entries(this.attributes)
+        .map(([attr, domain]) => attr in values);
+        const updateAttrStr = arrAttrs.map((attr) => `"${attr}"`)
+        const updateValuesStr = arrAttrs.map((attr) => {
+            const value = values[attr];
+            return typeof value === 'string' ? `'${value}'` : value;
+        });
+        const {rows} = await this.client.query(`
+        UPDATE ${this.tableName}
+        SET ${updateAttrStr} = ${updateValuesStr}
+        WHERE "id" = ${pkValue}
+        RETURNING *;`);
+        return rows;
+    }
+    static async deleteByPk(pkValue){
+        const {rows} = await this.client.query(`
+        DELETE
+        FROM ${this.tableName} 
+        WHERE "id"=${pkValue}
+        RETURNING *;`);
+        return rows;
+    }
+}
+
+module.exports = Thing;
